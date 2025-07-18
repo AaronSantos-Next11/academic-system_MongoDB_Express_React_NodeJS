a7d7b98ecb7f04ca1710a995fc30b52317b1ca5f
Aaron Santos <2302042@utrivieramaya.edu.mx>
2025-07-18 00:44:06 -0500

    feature: Añadir funcionalidad de usuario en app.js
    Se agrega el endpoint "careers",
    que conecta a la base de datos y se implementa
    la lógica para manejar las solicitudes de los usuarios.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 import express from 'express';
 import morgan from 'morgan';
-import dotenv from 'dotenv/config'
+import dotenv from 'dotenv/config';
 import connectDB from './connection/db.js';
 import cookieParser from 'cookie-parser';
+
+// Importar rutas del proyecto
 import authRoutes from './routes/auth.js';
+import careersRoutes from './routes/careers.routes.js';
 
-import careersRoutes from './routes/careers.routes.js'
-
+// Ejecuta la conexión a base de datos
 connectDB();
+
+// Instacia y uso de Express.js en este archivo
 const app = express();
 
+// Uso de dependencias
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
+// Se parte del endpoint "/api" y luego se alternan con las rutas de carreras o auth
 app.use('/api', authRoutes);
 app.use('/api', careersRoutes);
 
-export default app; // Exportamos la instancia de app, que se usa en este archivo
+export default app;
